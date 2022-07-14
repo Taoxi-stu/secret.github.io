9c3411dd9983f2ae130ddbaae89b258787a9c366
Senten <wuitenye@163.com>
2022-07-14 19:34:16 +0800

    添加图片轮播效果, 仍存在css样式重叠问题

--- js/picmove.js
+++ js/picmove.js
@@ -1,4 +1,3 @@
-
 var picFollowStatus = true;
 function isFollow(){
     if(picFollowStatus){
@@ -10,14 +9,40 @@ function isFollow(){
     }
 };
 function picFollow(e){
-    var x = e.pageX;
-    var y = e.pageY;
+    var x = Number(e.pageX);
+    var y = Number(e.pageY);
     let pic = document.getElementById('tyqh');
-    pic.style.left = x + 'px';
-    pic.style.top = y + 'px';   
-};
-function unFollow(){
-    console.log('111');            
-    document.getElementById('c').removeEventListener('mousemove',picFollow)
-};
+    let nx = Number(pic.style.left.substring(0,pic.style.left.lastIndexOf('px')));
+    let ny = Number(pic.style.top.substring(0,pic.style.top.lastIndexOf('px')));
+    var followTime = 20;
+    function delayFollow(i){
+        var tempx = nx + f1((x-nx)*i/followTime, Math.abs(x-nx));
+        var tempy = ny + f1((y-ny)*i/followTime, Math.abs(y-ny));
+        pic.style.left = String(tempx) + 'px';
+        pic.style.top = String(tempy)  + 'px';   //两个字符 '+8' 达到延时效果
+    }
+    // setInterval(delayFollow,100)
+    for(let i = 1; i<=followTime; i++){
+        setTimeout(delayFollow(i),10000)
+    }
 
+};
+function unFollow(){      
+    document.getElementById('c').removeEventListener('mousemove',picFollow);
+    let pic = document.getElementById('tyqh');
+    pic.style.left = '10px';
+    pic.style.top = '60%'
+
+};
+// function picChange(){
+//     let pic = document.getElementById('tyqh');
+//     // pic.draggable = true;
+//     // console.log('1111');
+//     pic.onmousedown = (e) => {
+//         e.target.style.left = e.pageX+ 'px';
+//         e.target.style.top = e.pageY+ 'px';
+//     }
+// }
+function f1(x,xmax){
+    return 1/Math.pow(xmax,2)*Math.pow(x,3);
+}
